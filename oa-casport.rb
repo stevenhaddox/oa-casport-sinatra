aa9f311105b8c5701bbdc7f348304dd23cb0b8aa
Steven Haddox <steven@haddox.us>
2011-07-02 09:36:55 -0400

    working sample app, time to refactor soon

--- oa-casport.rb
+++ oa-casport.rb
@@ -19,8 +19,7 @@ module OaCasportSinatra
     use OmniAuth::Builder do
       provider :casport, {
         :setup  => true,
-        :cas_server => 'http://cas.dev/users/'
-        #:cas_server => 'http://oa-cas.slkdemos.com/users/'
+        :cas_server => 'http://oa-cas.slkdemos.com/users/'
       }
     end
 
